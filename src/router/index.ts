010cfa5e8bfc87fac31c8308a3c6c678ab641e7b
a989464224 <3148584241@qq.com>
2026-06-28 09:51:11 +0800

    day2

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
@@ -39,6 +39,7 @@ const router = createRouter({
       name: 'board',
       component: () => import('@/views/BoardView.vue')
     },
+    { path: '/', redirect: '/home' }
   ],
 })
 
